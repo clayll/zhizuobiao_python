aa6a597e033e9a873cbd3ae10700042dd5828faa
liuliang <8238491@163.com>
2019-11-27 17:42:44 +0800

    update

--- 练习/day20/1.回家作业.py
+++ 练习/day20/1.回家作业.py
@@ -17,20 +17,20 @@ data.drop(['PassengerId','Name','Ticket','Cabin'],axis=1,inplace=True)
 data.Age = data.Age.fillna(data.Age.mean())
 data.dropna(subset=['Embarked'],how='any',axis=0,inplace=True)
 # print(data.isnull().sum())
-
-#数据规整
-# print(data.Sex.unique())
+#
+# #数据规整
+# # print(data.Sex.unique())
 data.loc[data['Sex']=='male','Sex']=0
 data.loc[data['Sex']=='female','Sex']=1
-
-# print(data.Embarked.unique())
-data.loc[data['Embarked']=='S','Embarked'] = 0
-data.loc[data['Embarked']=='C','Embarked'] = 1
-data.loc[data['Embarked']=='Q','Embarked'] = 2
-
-pd.options.display.max_rows=None
-pd.options.display.max_columns=None
-print(data.describe())
+#
+# # print(data.Embarked.unique())
+# data.loc[data['Embarked']=='S','Embarked'] = 0
+# data.loc[data['Embarked']=='C','Embarked'] = 1
+# data.loc[data['Embarked']=='Q','Embarked'] = 2
+#
+# pd.options.display.max_rows=None
+# pd.options.display.max_columns=None
+# print(data.describe())
 
 
 
@@ -47,22 +47,21 @@ print(data.describe())
 # mp.subplot(2,3,1)
 # data['Survived'].value_counts().plot(kind='bar',alpha=0.75,color=['dodgerblue','orangered'])
 # mp.title('存活人数')
-
-#3等舱人数最多，符合当时历史收入情况
+#
+# #3等舱人数最多，符合当时历史收入情况
 # mp.subplot(2,3,2)
 # data['Pclass'].value_counts().plot(kind='bar',alpha=0.75,color=['dodgerblue','orangered','limegreen'])
 # mp.title('不同船舱人数')
 #
-
-#舱位等级越高，获救比例越高。
+# #舱位等级越高，获救比例越高。
 # survive_0 = data['Pclass'][data['Survived']==0].value_counts()
 # survive_1 = data['Pclass'][data['Survived']==1].value_counts()
 # data2 = pd.DataFrame({'获救':survive_1,'未获救':survive_0})
 # data2.plot(kind='bar',stacked=True)
 # mp.title('不同舱位等级获救比例')
 # mp.show()
-
-
+#
+#
 # survive_0 = data['Embarked'][data['Survived']==0].value_counts()
 # survive_1 = data['Embarked'][data['Survived']==1].value_counts()
 # data2 = pd.DataFrame({'获救':survive_1,'未获救':survive_0})
@@ -70,11 +69,17 @@ print(data.describe())
 # mp.title('不同港口获救比例')
 
 #当时绅士精神，把存活机会留给了女士
+# 女性死亡获救的数量
 survive_0 = data['Survived'][data['Sex']==0].value_counts()
+# 男性死亡获救的数量
 survive_1 = data['Survived'][data['Sex']==1].value_counts()
-data2 = pd.DataFrame({'获救':survive_1,'未获救':survive_0})
-data2.plot(kind='bar',stacked=True)
+print(survive_0)
+print(survive_1)
+data2 = pd.DataFrame({'女性':survive_1,'男性':survive_0})
+print(data2)
+data2.plot(kind='bar')
 mp.title('不同性别获救比例')
+
 
 mp.show()
 
--- 练习/day20/homework.py
+++ 练习/day20/homework.py
@@ -9,12 +9,67 @@ mp.rcParams['axes.unicode_minus']=False
 
 
 pd.set_option('display.max_columns', 12)
-df = pd.read_csv('titanic_train.csv')
-
+# df = pd.read_csv('titanic_train.csv')
+#
+# print(df.head(2))
+#
+# print(df.loc[df.Embarked.isna() == True,'Embarked'].index.values)
+#
+# df1 = df.dropna(axis=0,subset=["Embarked"])
+#
+# print(df1['Embarked'].unique())
+df = pd.read_csv('douban250.csv')
 print(df.head(2))
+def checkName():
+    movieCount = len(df['movie_name'].unique())
+    print("数据总共有250条，检查重名的数量%s，所以没有重名" % movieCount)
+#a.	重复值检查（检查是否有重名电影）
+checkName()
 
-print(df.loc[df.Embarked.isna() == True,'Embarked'].index.values)
+def countryRank():
+    print(len(df[df['movie_country'].isnull()==True]))
+    print(len(df['movie_country'].unique()))
+    df1 = df.groupby(by='movie_country').size()
+    print(df1)
+countryRank()
+# b.	查看国家或地区与电影制作的排名情况（空值部分请替换为‘0’，可以考虑先按列计数，替换空值，再按行汇总。）
+# c.	统计每个区域里相同国家的总数。
+# d.	计算每个国家参与职做的电影总数排名情况
+# e.	获取电影类型数量前10的类型及上榜次数最多的导演
+# f.	评分和排名的关系a.	重复值检查（检查是否有重名电影）
+def myToInt(item):
+    print(item,type(item))
+    if isinstance(item,str):
+        return 1
+    else:
+        return item
 
-df1 = df.dropna(axis=0,subset=["Embarked"])
 
-print(df1['Embarked'].unique())
+
+contry = df['movie_country'].str.split(' ').apply(pd.Series)
+contry = contry.apply(pd.Series.value_counts)
+
+contry.fillna(value=0,inplace=True)
+
+contry.rename({0:'a1',1:'a2',2:'a3',3:'a4',4:'a5',5:'a6'},inplace=True,axis=1)
+
+
+print(contry)
+# x1 = contry.groupby('a1').sum()
+# print(x1)
+
+
+
+
+# s1 = df['movie_country'].str.split(' ')
+#
+#
+#
+# s2 = pd.Series(data= [['中国大陆', '中国香港'],['英国' ,'美国' ,'法国']],index=[2,3])
+# print(s2)
+
+# print(s1)
+
+# data = [[1,1,1,1,0]]
+# df2 = pd.DataFrame(data=data,index=['中国台湾-中国大陆-美国-中国香港'],columns=['中国台湾', '中国大陆', '美国', '中国香港','日本'])
+# print(df2)
