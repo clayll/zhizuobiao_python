f4e42ce58fd4d1237f17727bb3c23a767f0fc192
liuliang <8238491@163.com>
2019-12-05 17:56:20 +0800

    update

--- 练习/day20/homework.py
+++ 练习/day20/homework.py
@@ -3,15 +3,20 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as mp
+import matplotlib
 import seaborn as sns
 import math
+from wordcloud import WordCloud,STOPWORDS
+from PIL import Image
 #指定默认字体
-mp.rcParams['font.sans-serif'] = ['SimHei']
-mp.rcParams['font.family']='sans-serif'
-#解决负号'-'显示为方块的问题
-mp.rcParams['axes.unicode_minus'] = False
-sns.set(font='SimHei')  # 解决Seaborn中文显示问题  https://www.lizenghai.com/archives/34192.html
-
+# mp.rcParams['font.sans-serif'] = ['FangSong']
+# mp.rcParams['font.family']='FangSong'
+# # myfont = matplotlib.font_manager.FontProperties(fname='C:/Windows/Fonts/simhei.ttf')
+#
+# #解决负号'-'显示为方块的问题
+# mp.rcParams['axes.unicode_minus'] = False
+# sns.set(font='FangSong')  # 解决Seaborn中文显示问题  https://www.lizenghai.com/archives/34192.html
+print(matplotlib.matplotlib_fname())
 
 # pd.set_option('display.max_columns', 12)
 # df = pd.read_csv('titanic_train.csv')
@@ -44,6 +49,12 @@ def countryRank():
     contry = contry.apply(pd.Series.value_counts)
     contry.fillna(value=0, inplace=True)
     contry.rename({0: 'a1', 1: 'a2', 2: 'a3', 3: 'a4', 4: 'a5', 5: 'a6'}, inplace=True, axis=1)
+    contry = contry.sum(axis=1)
+    contry = pd.DataFrame({'国家名称':contry.index,'国家数量':contry.values})
+
+    print(contry)
+    sns.barplot(x='国家数量',y='国家名称',data=contry)
+    mp.show()
 
 # countryRank()
 
@@ -54,22 +65,42 @@ def countryTotal():
     contry.fillna(value=0, inplace=True)
     contry.rename({0: 'a1', 1: 'a2', 2: 'a3', 3: 'a4', 4: 'a5', 5: 'a6'}, inplace=True, axis=1)
     print(contry.sum(axis=1).sort_values(ascending=False))
+    sns.countplot(data=contry)
+    mp.show()
 
-cs = pd.cut(df['movie_num'],bins=12,labels=list("abcdefghijkl"))
-print(cs)
+# cs = pd.cut(df['movie_num'],bins=12,labels=list("abcdefghijkl"))
+# print(cs)
 
-countryTotal()
+# countryTotal()
 # d.	计算每个国家参与制作的电影总数排名情况
 # e.	获取电影类型数量前10的类型及上榜次数最多的导演
 def typeRank():
     types = df['movie_type'].str.split(' ').apply(pd.Series)
     types = types.apply(pd.Series.value_counts)
     types.fillna(value=0,inplace=True)
+    # 获取前10的类型
+    top10Types = pd.Series(types.sum(axis=1).sort_values(ascending=False),dtype=int).head(10)
+    top10index = top10Types.index.tolist()
+    def caculateTypeNum(item):
+        list = item['movie_type'].split(' ')
+        count = 0
+        for i in list:
+            if i in top10index:
+                count = count+1
+        item['actorTop'] = count
+        return item
+    df['actorTop'] = 0
 
-    print(pd.Series(types.sum(axis=1).sort_values(ascending=False),dtype=int) )
+    result = df.apply(caculateTypeNum,axis=1)
+    resuntgb = result.groupby(by='movie_director')[['actorTop']].sum()
+
+    resuntgb = resuntgb.sort_values(by='actorTop', ascending=False).head(10)
+    resuntgb = pd.DataFrame({"导演":resuntgb.index,"类型数量":resuntgb.values})
+    sns.barplot(x='类型数量',y='导演',data=resuntgb)
 
 
-typeRank()
+
+# typeRank()
 
 # f.	评分和排名的关系
 # print(np.corrcoef(df.movie_num ,df.movie_score))
@@ -77,8 +108,8 @@ typeRank()
 # mp.show()
 
 
-print(df.describe(include=['O']))
-print(df.corr())
+# print(df.describe(include=['O']))
+# print(df.corr())
 
 # x1 = contry.groupby('a1').sum()
 # print(x1)
@@ -124,16 +155,47 @@ def plot_distribution2(dataset : pd.DataFrame,cols=5,width=20,height=15,wspace=0
 
 
 
-print(type(df.columns))
 print([i for i in df.columns])
-print(math.ceil(float(df.shape[1]) / 3))
-# plot_distribution2(dataset=df.loc[:,['movie_country','movie_type']],height=100,cols=1)
-# mp.bar(['测试1','测试2','测试3'],[1,5,7])
-s = sns.regplot(x='movie_score',y='movie_num',data=df)
 
+s_mask = np.array(Image.open("stormtrooper_mask.png"))
+stopwords = set(STOPWORDS)
+stopwords.add("said")
+
+# wc = WordCloud(background_color="white", max_words=2000, mask=s_mask,
+#                stopwords=stopwords, contour_width=3, contour_color='steelblue')
+# mytext1 = ' '.join(df['movie_type'].tolist())
+mytext = ' '.join(df['movie_director'].tolist())
+wc1 = WordCloud(
+    background_color='white',  # 背景色
+    width=2000,  # 宽度
+    height=1000,  # 高度
+    font_path='simhei.TTF',  # 字体文件，此处与py文件放在同一目录
+mask=s_mask, stopwords=stopwords,
+    margin=1  # 词语边缘距离
+)
+wc2 = wc1.generate(mytext)  # 绘制词云
+mp.rcParams['figure.dpi'] = 600  # 修改dpi
+mp.rcParams['savefig.dpi']=600   # 修改dpi
+mp.axis('off')
+mp.imshow(wc2)
+
+# plt.savefig('{}.jpg'.format(filename))
 mp.show()
+
+
+
+# plot_distribution2(dataset=df.loc[:,['movie_year','movie_country','movie_type','movie_score']],height=30,cols=2)
+# mp.bar(['测试1','测试2','测试3'],[1,5,7])
+# s = sns.regplot(x='movie_score',y='movie_num',data=df)
+# 相关系数热力图
+# fig = mp.figure(figsize=(10,6))
+# fig.add_subplot(1,2,1)
+# sns.heatmap(data=df.corr(),cmap="YlOrBr_r")
+# fig.add_subplot(1,2,2)
+# sns.relplot(x='movie_score',y='movie_num',data=df)
+# mp.show()
 # s1 = df['movie_country'].str.split(' ')
-#
+
 #
 #
 # s2 = pd.Series(data= [['中国大陆', '中国香港'],['英国' ,'美国' ,'法国']],index=[2,3])
